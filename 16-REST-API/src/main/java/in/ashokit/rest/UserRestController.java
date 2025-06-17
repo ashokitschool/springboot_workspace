ec5bcd9b8c0c43ab557ab12b92802338cb3059da
Ashok <ashokitschool@gmail.com>
2025-06-17 07:52:13 +0530

    swagger changes

--- 16-REST-API/src/main/java/in/ashokit/rest/UserRestController.java
+++ 16-REST-API/src/main/java/in/ashokit/rest/UserRestController.java
@@ -18,7 +18,10 @@ import in.ashokit.dto.User;
 @RestController
 public class UserRestController {
 
-	@DeleteMapping(value = "/user/{userId}", produces = "text/plain")
+	@DeleteMapping(
+		value = "/user/{userId}", 
+		produces = "text/plain"
+	)
 	public ResponseEntity<String> deleteUser(@PathVariable Integer userId) {
 
 		// TODO : Delete user in Database
@@ -28,7 +31,14 @@ public class UserRestController {
 		return new ResponseEntity<>(msg, HttpStatus.OK);
 	}
 
-	@PutMapping(value = "/user/{userId}", consumes = "application/json", produces = "text/plain")
+	@PutMapping(
+		value = "/user/{userId}", 
+		consumes = {
+			"application/json",
+			"application/xml"
+		},
+		produces = "text/plain"
+	)
 	public ResponseEntity<String> updateUser(@RequestBody User user, @PathVariable Integer userId) {
 
 		System.out.println(user);
@@ -40,7 +50,14 @@ public class UserRestController {
 		return new ResponseEntity<>(msg, HttpStatus.OK);
 	}
 
-	@PostMapping(value = "/user", consumes = "application/json", produces = "text/plain")
+	@PostMapping(
+		value = "/user", 
+		consumes = {
+			"application/json",
+			"application/xml"
+		},
+		produces = "text/plain"
+	)
 	public ResponseEntity<String> createUser(@RequestBody User user) {
 
 		System.out.println(user);
@@ -52,7 +69,13 @@ public class UserRestController {
 		return new ResponseEntity<>(msg, HttpStatus.CREATED);
 	}
 
-	@GetMapping(value = "/user", produces = "application/json")
+	@GetMapping(
+		value = "/user", 
+		produces = {
+			"application/json",
+			"application/xml"
+		}
+	)
 	public ResponseEntity<User> getUser() {
 
 		User user = new User(101, "Raj", "raj@gmail.com");
@@ -60,7 +83,13 @@ public class UserRestController {
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 
-	@GetMapping(value = "/users", produces = "application/json")
+	@GetMapping(
+		value = "/users", 
+		produces = {
+			"application/json",
+			"application/xml"
+		}
+	)
 	public ResponseEntity<List<User>> getUsers() {
 
 		User u1 = new User(101, "Raj", "raj@gmail.com");
